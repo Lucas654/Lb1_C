2fc4abc83c3d95e01816d9458c4498dbe6c0ce73
lucaa <lucaa@DESKTOP-KSUOT9E>
2022-12-25 23:45:12 +0200

    Добавьте файлы проекта.

---
+++ lbb1/BankAccount.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lbb1
+{
+    class BankAccount
+    {
+        public string FirstName;
+        public string LastName;
+        public string MiddleName;
+        public string ID;
+
+        public Dictionary<double, string> mass = new Dictionary<double, string>();
+
+        enum Depos
+        {
+
+            Depos1 = 14,
+            Depos2 = 12,
+            Depos3 = 19
+        }
+        public double Rah = 0;
+
+        public BankAccount(string LastName, string FirstName, string MiddleName, string ID)
+        {
+            this.LastName = LastName;
+            this.FirstName = FirstName;
+            this.MiddleName = MiddleName;
+            this.ID = ID;
+        }
+
+        public void AddDepos(string NameDep, double money)
+        {
+
+            if (NameDep == Depos.Depos1.ToString())
+            {
+                mass.Add(money, NameDep);
+                Console.WriteLine($"Депозит {NameDep} відкрито");
+            }
+            else if (NameDep == Depos.Depos2.ToString())
+            {
+                mass.Add(money, NameDep);
+                Console.WriteLine($"Депозит {NameDep} відкрито");
+            }
+            else if (NameDep == Depos.Depos3.ToString())
+            {
+                mass.Add(money, NameDep);
+                Console.WriteLine($"Депозит {NameDep} відкрито");
+            }
+            else
+            {
+                Console.WriteLine("Невірна назва депозиту");
+            }
+
+        }
+
+        public double Income(string NameDep)
+        {
+            double m = 0;
+            foreach (var dep in mass)
+            {
+                if (dep.Value == NameDep)
+                {
+                    if (NameDep == Depos.Depos1.ToString())
+                    {
+                        
+                        m = dep.Key + dep.Key * ((double)Depos.Depos1/100f);
+                        return m;
+
+                    }
+                    else if (NameDep == Depos.Depos2.ToString())
+                    {
+                        m = dep.Key+dep.Key * ((double)Depos.Depos2 / 100f);
+                        return m;
+                    }
+                    else if (NameDep == Depos.Depos3.ToString())
+                    {
+                        m = dep.Key + dep.Key * ((double)Depos.Depos3 / 100f);
+                        return m;
+                    }
+
+                }
+            }
+            return m;
+
+        }
+
+        public void TopUp(double money)
+        {
+            Rah += money;
+            Console.WriteLine($"Рахунок поповнено на сумму: {money}");
+
+        }
+        public void Windraw(double money)
+        {
+            if(Rah>money)
+            {
+                Rah -= money;
+                Console.WriteLine($"З рахунку знято грошей на сумму: {money}");
+            }
+            
+        }
+
+        public void CloseDep(string namedep)
+        {
+            foreach (var dep in mass)
+            {
+                if (dep.Value == namedep)
+                {
+                    Rah += Income(namedep);
+                    mass.Remove(dep.Key);
+                    Console.WriteLine($"Депозит {namedep} закрито");
+                    break;
+                }
+                else
+                    Console.WriteLine($"Депозиту {namedep} неіснує");
+            }
+        }
+
+        public void AllMoney()
+        {
+            double all=0;
+            foreach(var dep in mass)
+            {
+                all += dep.Key;
+            }
+            Console.WriteLine($"Грошей на депозитах: {all}");
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Призвіще: {0} , Ім'я: {1}, По-батькові: {2}, Номер рахунку: {3}, Кількість депозитів: {4}, Рахунок:{5}", this.LastName,this.FirstName,this.MiddleName,this.ID,this.mass.Count.ToString(),this.Rah.ToString());
+        }
+
+
+
+    }
+}
---
+++ lbb1/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lbb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<BankAccount> accounts = new List<BankAccount>();
+            var action="";
+            while (action != "0")
+            {
+                Console.WriteLine("Вас вітає Банк№1");
+                Console.WriteLine("Що бажаєте зробити:");
+                Console.WriteLine("1 - Створити рахунок");
+                Console.WriteLine("2 - Зайди до особистого рахунку");
+                Console.WriteLine("0 - Вихід");
+                action = Console.ReadLine();
+                switch (action)
+                {
+                    case "1":
+                        string ln, fn, mn, id;
+                        Console.WriteLine("Введіть призвіще:");
+                        ln = Console.ReadLine();
+                        Console.WriteLine("Введіть ім'я:");
+                        fn = Console.ReadLine();
+                        Console.WriteLine("Введіть ім'я по-батькові:");
+                        mn = Console.ReadLine();
+                        Console.WriteLine("Введіть id майбутнього рахунку, 8 символів:");
+                        id = Console.ReadLine();
+                        if (ln != string.Empty && fn != string.Empty && mn != string.Empty && int.TryParse(id, out _))
+                        {
+                            if (accounts.Count > 0)
+                            {
+                                for (int i = 0; i < accounts.Count; i++)
+                                {
+                                    if (accounts[i].ID != id)
+                                    {
+                                        accounts.Add(new BankAccount(ln, fn, mn, id));
+                                        Console.WriteLine("Рахунок створено");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Рахунок з даним ID вже зареєстрований");
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                accounts.Add(new BankAccount(ln, fn, mn, id));
+                                Console.WriteLine("Рахунок створено");
+                            }    
+                        }
+                        else
+                            action = "1";
+                        break;
+                    case "2":
+                        string ID;
+                        Console.WriteLine("Введіть ID рахунку:");
+                        ID = Console.ReadLine();
+
+                        for (int i = 0; i < accounts.Count; i++)
+                        {
+                            if (accounts[i].ID == ID)
+                            {
+                                string action2 = "";
+                                while (action2 != "0")
+                                {
+                                    Console.WriteLine("Вітаємо Вас");
+                                    Console.WriteLine(accounts[i].ToString());
+                                    Console.WriteLine("Що бажаєте зробити:");
+                                    Console.WriteLine("1 - Відкрити депозит");
+                                    Console.WriteLine("2 - Поповнити рахунок");
+                                    Console.WriteLine("3 - Зняти готівку");
+                                    Console.WriteLine("4 - Закрити депозит");
+                                    Console.WriteLine("5 - Загальна кількість коштів на депозитах");
+                                    Console.WriteLine("0 - Вихід");
+                                    action2 = Console.ReadLine();
+                                    switch(action2)
+                                    {
+                                        case "1":
+                                            Console.WriteLine("Оберіть депозит");
+                                            Console.WriteLine("Depos1");
+                                            Console.WriteLine("Depos2");
+                                            Console.WriteLine("Depos3");
+                                            var a = Console.ReadLine();
+                                            Console.WriteLine("Введіть сумму депозиту:");
+                                            var b = Console.ReadLine();
+                                            if (a != string.Empty && double.Parse(b) > 0)
+                                            {
+                                                accounts[i].AddDepos(a, double.Parse(b));
+                                            }
+                                            break;
+                                        case "2":
+                                            Console.WriteLine("Введіть суму поповнення:");
+                                            var mon = Console.ReadLine();
+                                            if (double.TryParse(mon, out double m))
+                                            {
+                                                accounts[i].TopUp(m);
+                                            }
+                                            else
+                                                Console.WriteLine("Помилка");
+                                            break;
+                                        case "3":
+                                            Console.WriteLine("Введіть суму зняття:");
+                                            var Mon = Console.ReadLine();
+                                            if (double.TryParse(Mon, out m))
+                                            {
+                                                accounts[i].Windraw(m);
+                                            }
+                                            break;
+                                        case "4":
+                                            foreach (var D in accounts[i].mass)
+                                                Console.WriteLine(D.Value);
+                                            Console.WriteLine("Оберіть депозит для закрыття");
+                                            var d = Console.ReadLine();
+                                            accounts[i].CloseDep(d);
+                                            break;
+                                        case "5":
+                                            accounts[i].AllMoney();
+                                            break;
+                                    }
+
+                                }
+
+
+                            }
+                            else
+                                Console.WriteLine("Такого рахунку не існує");
+
+                        }
+
+                        break;
+                }
+            }
+
+           
+
+        }
+    }
+}
